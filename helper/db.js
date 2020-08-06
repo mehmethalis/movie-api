35ea8a1db91cd02579a6743329bd23ee90c64477
Mehmet Halis <mehmethaliscicek@gmail.com>
2020-08-06 19:35:52 +0300

    writing unit tests

--- helper/db.js
+++ helper/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
 
-    mongoose.connect("mongodb+srv://firstUser:db1234@movieapi.9dhha.mongodb.net/movieAPI?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    mongoose.connect("mongodb://firstUser:db1234@movieapi-shard-00-00.9dhha.mongodb.net:27017,movieapi-shard-00-01.9dhha.mongodb.net:27017,movieapi-shard-00-02.9dhha.mongodb.net:27017/movieAPI?ssl=true&replicaSet=atlas-12wne5-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
         console.log('db connected...');
     }).catch((err) => {
         console.log('db connnection failed : ' + err);
--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
 const server = require('../../app');
+const { token } = require('morgan');
 
 chai.use(chaiHttp);
 
@@ -26,4 +27,96 @@ describe('Movies directs tests', () => {
 
     });
 
+
+});
+
+
+describe('(POST/) Movie', () => {
+    let token;
+    before((done) => {
+        chai.request(server).post('/authenticate').send({ userName: 'ozan', password: '12345' }).end((err, res) => {
+            token = res.body.token;
+            console.log('TOKEN : ' + token + '\n');
+            done();
+        });
+    });
+    it('it should post a movie', (done) => {
+        let movie1 = {
+            director_id: '5f2854f38b925d0b5038dc43',
+            title: 'udem',
+            imdb: 8,
+            category: 'comedy',
+            country: 'usa',
+            year: 1950
+        };
+
+        chai.request(server).post('/api/movies').set('x-access-token', token).send(movie1).end((err, res) => {
+
+            if (err) {
+                console.log(err);
+            }
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('status');
+            done();
+        });
+    });
+});
+
+describe('(GET/:director_id) Movie', () => {
+    let token;
+    before((done) => {
+        chai.request(server).post('/authenticate').send({ userName: 'ozan', password: '12345' }).end((err, res) => {
+            token = res.body.token;
+            console.log('TOKEN : ' + token + '\n');
+            done();
+        });
+    });
+    it('it should get a movie with director_id', (done) => {
+
+        const movie_id = '5f2855e189a3780f1c20e285';
+        chai.request(server).get('/api/movies/' + movie_id).set('x-access-token', token).end((err, res) => {
+
+            if (err) {
+                console.log(err);
+            }
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            done();
+        });
+    });
+});
+
+
+describe('(PUT/:director_id) Movie', () => {
+    let token;
+    before((done) => {
+        chai.request(server).post('/authenticate').send({ userName: 'ozan', password: '12345' }).end((err, res) => {
+            token = res.body.token;
+            done();
+        });
+    });
+    it('it should update a movie with director_id', (done) => {
+        let movie = {
+            director_id: '5f2854f38b925d0b5038dc43',
+            title: 'udemy update',
+            imdb: 8,
+            category: 'comedy',
+            country: 'usa',
+            year: 1950
+
+        };
+
+        let movie_id1 = '5f2c2a89ca1548272cd51b6b';
+        chai.request(server).put('/api/movies/' + movie_id1).set('x-access-token', token).send(movie).end((err, res) => {
+
+            if (err) {
+                console.log(err);
+            }
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('title').eql(movie.title);
+            done();
+        });
+    });
 });

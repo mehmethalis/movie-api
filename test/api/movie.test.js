f15287c6cc9e9ba2a36e618aa2697080a49762b7
Mehmet Halis <mehmethaliscicek@gmail.com>
2020-08-07 14:43:30 +0300

    tests updated

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -86,37 +86,3 @@ describe('(GET/:movie_id) Movie', () => {
         });
     });
 });
-
-
-describe('(PUT/:movie_id) Movie', () => {
-    let token;
-    before((done) => {
-        chai.request(server).post('/authenticate').send({ userName: 'ozan', password: '12345' }).end((err, res) => {
-            token = res.body.token;
-            done();
-        });
-    });
-    it('it should update a movie with movie_id', (done) => {
-        let movie = {
-            director_id: '5f2854f38b925d0b5038dc43',
-            title: 'udemy update',
-            imdb: 8,
-            category: 'comedy',
-            country: 'usa',
-            year: 1950
-
-        };
-
-        let movie_id1 = '5f2c2a89ca1548272cd51b6b';
-        chai.request(server).put('/api/movies/' + movie_id1).set('x-access-token', token).send(movie).end((err, res) => {
-
-            if (err) {
-                console.log(err);
-            }
-            res.should.have.status(200);
-            res.body.should.be.a('object');
-            res.body.should.have.property('title').eql(movie.title);
-            done();
-        });
-    });
-});
